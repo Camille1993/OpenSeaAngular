099ff73534e757dc8b8a1eaf9f395ce0e238eb05
Camille Hautois <camille.hautois@gmail.com>
2022-02-28 17:37:49 +0100

    add provider metamask to app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'openSea-Angular';
+
+  async connect() {
+    const provider = (window as any).ethereum;
+    if (!provider) alert ('Create your Metamask account!');
+    await provider.request({method: 'eth_requestAccounts'}); 
+  }
 }
